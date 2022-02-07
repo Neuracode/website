025bb25ffd0c00a07d1c659dfef60f45b11f85c8
Azam Ahmed <aziskool20@gmail.com>
2022-02-06 20:15:57 -0500

    Home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar bg='light' variant='light' id='navbar'>
+        <Container>
+          <Navbar.Brand href='#home'>SynapticAI</Navbar.Brand>
+          <Nav className='ms-auto'>
+            <Nav.Link href='#courses'>Courses</Nav.Link>
+            <Nav.Link href='#about'>About</Nav.Link>
+            <Nav.Link href='#contact'>Contact Us</Nav.Link>
+            <Nav.Link href='#donate'>Donate</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
