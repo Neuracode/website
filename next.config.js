ef39909cba25b2b1e4d2f0072008e21597e7ed22
Azam Ahmed <aziskool20@gmail.com>
2022-02-20 15:06:44 -0500

    Update deploy.yml and next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
     domains: ['img.icons8.com'],
     loader: 'akamai',
     path: '/'
-  }
+  },
+  basePath: '/website',
+  assetPrefix: '/website'
 }
 
 module.exports = nextConfig
