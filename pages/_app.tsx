fb3319e349f7b48fe506ef7f340422b9a36b8177
Azam Ahmed <aziskool20@gmail.com>
2022-05-11 20:27:21 -0400

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,28 @@ import Link from 'next/link';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as ga from '../lib/ga';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            ga.pageview(url);
+        };
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
     useEffect(() => {
         AOS.init({
             duration: 1000,
