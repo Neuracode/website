009821fbfaa53cf5685d08f45e767d9c14e2b90a
Azam Ahmed <aziskool20@gmail.com>
2022-05-15 13:18:36 -0400

    Add Jaiman as director of outreach

--- pages/about/team.tsx
+++ pages/about/team.tsx
@@ -4,31 +4,37 @@ import styles from '../../styles/About.module.css';
 import TeamCard from '../../components/TeamCard';
 
 const Team: React.FC = () => {
-  return (
-    <>
-      <Container>
-        <h1 className={styles.title}>Team</h1>
-        <h3 className={styles.topText}>
-          Executive Board
-        </h3>
-        <div className='mt-5 mb-5 d-flex flex-row flex-wrap justify-content-around'>
-          <TeamCard
-            name='Azam Ahmed'
-            title='Founder + President'
-            image='azam.jpg'
-            description='High school student who loves to code and delve deeper
+    return (
+        <>
+            <Container>
+                <h1 className={styles.title}>Team</h1>
+                <h3 className={styles.topText}>Executive Board</h3>
+                <div className='mt-5 mb-5 d-flex flex-row flex-wrap justify-content-around'>
+                    <TeamCard
+                        name='Azam Ahmed'
+                        title='Founder + President'
+                        image='azam.jpg'
+                        description='High school student who loves to code and delve deeper
             into the field of machine learning.'
-            linkedin='https://www.linkedin.com/in/azam-ahmed'
-            github='https://github.com/azam999'
-            website='https://azamahmed.tech'
-          />
-        </div>
-        <h3 className={styles.topText}>
-          Interns
-        </h3>
-      </Container>
-    </>
-  );
+                        linkedin='https://www.linkedin.com/in/azam-ahmed'
+                        github='https://github.com/azam999'
+                        website='https://azamahmed.tech'
+                    />
+                </div>
+                <h3 className={styles.topText}>Directors</h3>
+                <div className='mt-5 mb-5 d-flex flex-row flex-wrap justify-content-around'>
+                    <TeamCard
+                        name='Jaiman Pandya'
+                        title='Director of Outreach'
+                        image='jaiman_pandya.jpeg'
+                        description='Student researcher in machine learning with a passion for computational biology'
+                        linkedin='https://www.linkedin.com/in/jaiman-pandya-825350234'
+                    />
+                </div>
+                <h3 className={styles.topText}>Interns</h3>
+            </Container>
+        </>
+    );
 };
 
 export default Team;
