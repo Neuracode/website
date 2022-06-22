6572fa01dedacb451e588c3f7ee51c3075e01251
Azam Ahmed <78384818+Azam999@users.noreply.github.com>
2022-06-21 21:11:46 -0400

    Change Java DS date

--- pages/courses/java-data-structures/index.tsx
+++ pages/courses/java-data-structures/index.tsx
@@ -24,7 +24,7 @@ const JavaDataStructures: React.FC = ({ }) => {
                 name='Java Data Structures'
                 curriculumList={curriculumList}
                 about='If you have already learned the basics of Java and are looking to get farther into your computer science journey, then this is the course for you. This course will teach you all of the basic data structures in Java but requires you to have basic knowledge of Java, including knowledge of arrays, array lists, and how a class is structured.'
-                startDate='June 30th'
+                startDate='July 8th'
                 endDate='August 30th'
                 dayAndTime='Friday, 3:00PM - 4:30PM + Saturday, 3:00PM - 4:30PM'
                 timeCommitment={3}
