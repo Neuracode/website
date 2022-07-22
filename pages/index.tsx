f7001b539bfd1b14cebcf6f9b1ce3e623a457872
Azam Ahmed <78384818+Azam999@users.noreply.github.com>
2022-07-22 18:37:59 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,7 +168,7 @@ const Home: NextPage = () => {
                         <div className={styles.statisticText}>Impressions</div>
                     </div>
                     <div>
-                        <div className={styles.statistic}>9</div>
+                        <div className={styles.statistic}>62</div>
                         <div className={styles.statisticText}>Countries</div>
                     </div>
                     <div>
